449532b7fd74040dfafdef1b534f59a4d526bbb5
Jaffdavy <jaffdavy100@gmail.com>
2024-02-05 15:20:15 +0100

    correcting lint errors

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
-function generateSecretHandshake() {
-    const numberInput = parseInt(document.getElementById('numberInput').value)
-    if (!numberInput || numberInput < 0) {
-        alert('Please enter a valid non-negative number.')
-        return;
-    }
+function generateSecretHandshake () {
+  const numberInput = parseInt(document.getElementById('numberInput').value)
+  if (!numberInput || numberInput < 0) {
+    alert('Please enter a valid non-negative number.')
+    return
+  }
 
-    const binaryRepresentation = numberInput.toString(2); // Convert the number to binary
-    const handshake = decodeBinary(binaryRepresentation)
+  const binaryRepresentation = numberInput.toString(2); // Convert the number to binary
+  const handshake = decodeBinary(binaryRepresentation)
 
-    const secretHandshakeElement = document.getElementById('secretHandshake')
-    secretHandshakeElement.textContent = `Secret Handshake: ${handshake.join(', ')}`
+  const secretHandshakeElement = document.getElementById('secretHandshake')
+  secretHandshakeElement.textContent = `Secret Handshake: ${handshake.join(', ')}`
 }
 
-function decodeBinary(binaryString) {
-    const handshakeActions = [
-        "wink",
-        "double blink",
-        "close your eyes",
-        "jump",
-        "Reverse the order"
-    ];
+function decodeBinary (binaryString) {
+  const handshakeActions = [
+    'wink',
+    'double blink',
+    'close your eyes',
+    'jump',
+    'Reverse the order'
+  ]
 
-    const decodedHandshake = []
+  const decodedHandshake = []
 
-    for (let i = 0; i < binaryString.length; i++) {
-        if (binaryString[i] === '1') {
-            if (handshakeActions[i] === "Reverse the order") {
-                decodedHandshake.reverse();
-            } else {
-                decodedHandshake.push(handshakeActions[i])
-            }
-        }
+  for (let i = 0; i < binaryString.length; i++) {
+    if (binaryString[i] === '1') {
+      if (handshakeActions[i] === "Reverse the order") {
+        decodedHandshake.reverse()
+      } else {
+        decodedHandshake.push(handshakeActions[i])
+      }
     }
+  }
 
-    return decodedHandshake
+  return decodedHandshake
 }
+generateSecretHandshake()
